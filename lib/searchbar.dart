d815c2db698757a41f6fbdf111c7abf6e68d3698
abedhamiyeh <abedhamye28@gmail.com>
2023-12-28 19:56:42 +0200

    layout adjustments

--- lib/searchbar.dart
+++ lib/searchbar.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -11,60 +10,58 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   bool isSearchVisible = false;
+  final List<IconData> icons = [
+    Icons.headphones,
+    Icons.laptop,
+    Icons.phone_android,
+    Icons.computer,
+    Icons.keyboard_alt_outlined,
+  ];
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(10),
+          padding: EdgeInsets.only(top: 7),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-
-
-
-                    const Column(
+              Container(
+                width: screenWidth,
+                color: Colors.blue.shade900,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Center(
-                          child:
-                          Row(
-                            children: [
-                              Text("Tech-Pro", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),)
-                            ],
+                        Text(
+                          "Tech-Pro",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.white,
                           ),
-                        )
-
-
-
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.search, color: Colors.white, size: 30),
+                          onPressed: () {
+                            setState(() {
+                              isSearchVisible = !isSearchVisible;
+                            });
+                          },
+                        ),
                       ],
                     ),
-
-
-                  IconButton(
-                    icon: Icon(Icons.search, color: Colors.blue.shade400, size: 30,),
-                    onPressed: () {
-                      setState(() {
-                        isSearchVisible = !isSearchVisible;
-                      });
-                    },
-                  ),
-                ],
-              ),
-
-              Visibility(
-                visible: isSearchVisible,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.center,
+                    SizedBox(height: 10),
+                    Visibility(
+                      visible: isSearchVisible,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
                         child: TextField(
                           style: TextStyle(color: Colors.black),
                           decoration: InputDecoration(
@@ -81,81 +78,85 @@ class _SearchState extends State<Search> {
                         ),
                       ),
                     ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: icons.map((icon) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            padding: EdgeInsets.all(12),
+                            child: Icon(
+                              icon,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
                   ],
                 ),
               ),
               SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(color: Colors.blue.shade500,
-
-                    borderRadius: BorderRadius.circular(20)
+              CarouselSlider(
+                options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  autoPlay: true,
                 ),
-                height: 200,
-                  child:Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0), // Adjust the border radius as needed
-                        child: Image.asset(
-                          "assets/tech.jpg",
-                          height: 120.0,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ],
-                  )
-
+                items: [
+                  Image.network("https://cdn.zeebiz.com/sites/default/files/styles/zeebiz_850x478/public/2022/12/15/216399-christmas-discounts.jpg?itok=LQW4C3JY"),
+                  Image.network("https://www.91-cdn.com/hub/wp-content/uploads/2023/10/image1-2.png", fit: BoxFit.cover),
+                  Image.network("https://www.91-cdn.com/hub/wp-content/uploads/2023/10/image1-2.png", fit: BoxFit.cover),
+                ],
               ),
               SizedBox(height: 20),
-                Row(
-                children: [
-                Container(
-                decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
+              Container(
+                height: 50,
+                padding: EdgeInsets.only(right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("View More", style: TextStyle(color: Colors.white)),
+                    Icon(Icons.arrow_circle_right, color: Colors.white),
+                  ],
                 ),
-                height: 100,
-                width: 100,
-                child: Image.asset(
-                "assets/img1.jpg",
-                fit: BoxFit.cover,
-                ),
-                ),
-                SizedBox(width: 20),
-                Container(
-                decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                ),
-                height: 100,
-                width: 100,
-                child: Image.asset(
-                "assets/img1.jpg", // Replace with appropriate image path
-                fit: BoxFit.cover,
-                ),
-                ),
-                SizedBox(width: 20),
-                Container(
-                decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                ),
-                height: 100,
-                width: 100,
-                child: Image.asset(
-                "assets/img1.jpg", // Replace with appropriate image path
-                fit: BoxFit.cover,
-                ),
-                ),
-                ],
-                ),
-
+              ),
+              SizedBox(height: 20),
+              buildProductRow("https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/MSFT-surfacelaptopgo2-mobile-hero-RE4Ypdj?scl=1", "https://static.skyassets.com/contentstack/assets/blt143e20b03d72047e/blt1c33e1158f1c5ecf/6319d97c454b1c2ebb3f4037/Carousel_iPhone14Plus_Purple_Placement01-PreOrder.png"),
+              SizedBox(height: 20),
+              buildProductRow("https://www.tomorrowsworldtoday.com/wp-content/uploads/2023/01/Image3-26.jpg", "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images2.zoodmall.com/https%3A/www.higalo.com/DynamicImages/ProductsImages/Product-Media-Pic-405-637486349617672328.jpg"),
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget buildProductRow(String imageUrl1, String imageUrl2) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildProductContainer(imageUrl1),
+        SizedBox(width: 20),
+        buildProductContainer(imageUrl2),
+      ],
+    );
+  }
+
+  Widget buildProductContainer(String imageUrl) {
+    return Container(
+      color: Colors.white,
+      height: 150,
+      width: 150,
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
 }
-
-
